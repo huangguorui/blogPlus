1d826246373848ff2c7ba701fe491b82a7fd3af4
huangguorui <2315684336@qq.com>
2019-01-28 17:59:59 +0800

    积分规则 vip昵称

--- src/views/user.vue
+++ src/views/user.vue
@@ -7,7 +7,10 @@
          style="color:green;text-align:center">
         <Icon type="ios-information-circle"></Icon>
         <span>恭喜成功升级到VIP{{vipLevel}}</span>
-        <span>获得称号{{vipLevel}}</span>
+      </p>
+      <p slot="header"
+         style="color:green;text-align:center">
+        <span>获得称号【{{nickName[vipLevel-1].text}}】称号</span>
       </p>
       <div>
         <p>您已经成功解锁如下功能</p>
@@ -47,10 +50,40 @@ export default {
       }, {
         id: 3,
         text: ['黑夜护眼功能333', '界面文章字体大小自定义333', '文字颜色修改333']
+      }],
+      nickName: [{
+        text: '倔强青铜'
+      }, {
+        text: '持续白银'
+      }, {
+        text: '荣耀黄金'
+      }, {
+        text: '尊贵铂金'
+      }, {
+        text: '永恒钻石'
+      }, {
+        text: '最强王者'
+      }, {
+        text: '荣耀王者'
       }]
-
     }
   },
+  /*
+小萌新
+倔强青铜 
+持续白银 
+荣耀黄金 
+尊贵铂金 
+永恒钻石 
+最强王者 
+荣耀王者
+
+特殊VIP徽章 黑夜护眼 字体大小自定义 文章颜色自定义 每次登陆经验值+10 每日免费转盘一次
+特殊VIP徽章 音乐播放 每次登陆经验值+15 每日免费转盘一次
+特殊VIP徽章 页面背景自定义颜色 每次登陆经验值+20
+特殊VIP徽章 每次登陆经验值+25
+
+*/
   methods: {
     del () {
       this.modal_loading = true;
